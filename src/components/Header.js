2ec5bf04ed424815bfcaef34dfb5a7656bf5d9dc
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-10 00:23:23 +0200

    Header menu + sass-config

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,15 @@ function Header(props) {
     return (
         <div className="header">
             <div className="header-container">
-                <div className="header-container__logo">
+                <a href="/" className="header-container__logo">
                     Coders<span className="header-container__logo-element">KK</span>
-                </div>
+                </a>
 
                 <nav className="header-container__nav">
                     <a href="/">Home</a>
-
-                        <a href="/myApps">My Apps</a>
-                        <a href="/contact">Contact</a>
-
+                    <a href="/myApps">My Apps</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/signIn">Zaloguj</a>
                 </nav>
 
             </div>
