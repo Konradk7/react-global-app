ea3a2a59a246b720b36e216caaef08220dd3f0d2
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-09 21:47:36 +0200

    Init sass and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import Header from "./components/Header";
 
 
 function App(props) {
     return (
-        <div></div>
+        <div>
+            <Header />
+        </div>
     );
 }
 
