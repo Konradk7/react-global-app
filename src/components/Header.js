1d889447aa288650af87c42d9b1e7c6479a8afbd
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-09 22:32:04 +0200

    Init a little bit header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 function Header(props) {
     return (
-        <div className="Header"></div>
+        <div className="header">
+            <div className="header-container">
+                <div className="header-container__logo">
+                    Coders<span className="header-container__logo-element">KK</span>
+                </div>
+
+                <nav className="header-container__nav">
+                    <a href="/">Home</a>
+
+                        <a href="/myApps">My Apps</a>
+                        <a href="/contact">Contact</a>
+
+                </nav>
+
+            </div>
+        </div>
     );
 }
 
