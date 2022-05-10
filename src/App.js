9c4c9bae0adc32c5deda3fec5669bfc76f173a11
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-10 02:33:36 +0200

    Add main + main-colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './sass/App.scss';
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 
 function App(props) {
     return (
         <div>
             <Header />
+            <Main />
         </div>
     );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main(props) {
+    return (
+        <div className="main">
+            <div className="main-container">
+
+            </div>
+        </div>
+    );
+}
+
+export default Main;
