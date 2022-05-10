43b5a52a0f1ee0ddc8af4d7252aac1c13c598b54
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-11 00:28:27 +0200

    add styles to text-form

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -5,15 +5,21 @@ function WeatherForm({value, change, submit}) {
 
         <form onSubmit={submit} className="weather-form">
             <div className="weather-form-container">
-                <h2 className="weather-form-container__title"> Add Your weather-widget to Home for make Your live more easier!</h2>
-                <p> Just write city which you are interesting for below and always see everything what will You need!</p>
-                <span>Hour</span>
-                <span>Temperature</span>
-                <span>Sunset</span>
-                <p>Even more! Your widget will show You every information's by quickly icons! Right now You don't need to open slowly
+                <h2 className="weather-form-container__title"> Add Your weather-widget to Home for make Your live more
+                    easier!</h2>
+                <p className="weather-form-container__text"> Just write city which you are interesting for below and
+                    always see everything what will You need!</p>
+                <ul className="weather-form-container__list">
+                    <li className="weather-form-container__list-element">Hour</li>
+                    <li className="weather-form-container__list-element">Temperature</li>
+                    <li className="weather-form-container__list-element">Sunset</li>
+                </ul>
+                <p className="weather-form-container__text">Even more! Your widget will show You every information's by
+                    quickly icons! Right now You don't need to open slowly
                     core-app with tons of useless option's. Everything with you just through quick look!
                 </p>
-                <p>Combine it with our another widgets for keeping all useful tools in one place. It's your valuable time!</p>
+                <p className="weather-form-container__footer">Combine it with our another widgets for keeping all useful tools in one place. It's your valuable
+                    time!</p>
             </div>
             <input
                 type="text"
