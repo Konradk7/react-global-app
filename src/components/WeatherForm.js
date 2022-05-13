5d71bcf639a187b004fce42557c0afdca5edd5aa
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-13 18:11:41 +0200

    Rebuild styles form

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -7,19 +7,24 @@ function WeatherForm({value, change, submit}) {
             <div className="weather-form-container">
                 <h2 className="weather-form-container__title"> Add Your weather-widget to Home for make Your live more
                     easier!</h2>
-                <p className="weather-form-container__text"> Just write city which you are interesting for below and
-                    always see everything what will You need!</p>
-                <ul className="weather-form-container__list">
-                    <li className="weather-form-container__list-element">Hour</li>
-                    <li className="weather-form-container__list-element">Temperature</li>
-                    <li className="weather-form-container__list-element">Sunset</li>
-                </ul>
-                <p className="weather-form-container__text">Even more! Your widget will show You every information's by
-                    quickly icons! Right now You don't need to open slowly
+
+                <p className="weather-form-container__text">
+                    <ul className="weather-form-container__list">
+                        <li className="weather-form-container__list-element">Hour</li>
+                        <li className="weather-form-container__list-element">Temperature</li>
+                        <li className="weather-form-container__list-element">Sunset</li>
+                    </ul>
+                    <p className="weather-form-container__text-main">
+                    Even more! Your widget will show You every information's by
+                    icons! Right now You don't need to open slowly
                     core-app with tons of useless option's. Everything with you just through quick look!
+                    </p>
+                    <p className="weather-form-container__text-footer"> Just write city which you are interesting for below.</p>
+
                 </p>
-                <p className="weather-form-container__footer">Combine it with our another widgets for keeping all useful tools in one place. It's your valuable
-                    time!</p>
+
+                <p className="weather-form-container__footer">Made by KK </p>
+
             </div>
             <input
                 type="text"
