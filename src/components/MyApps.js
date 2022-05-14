85a932a43074dc47f979e2ed510564a370874de9
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-14 11:05:48 +0200

    init slider-styles

--- src/components/MyApps.js
+++ src/components/MyApps.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Weather from "./Weather";
+import arrow from "../images/arrow.png";
 
 function MyApps(props) {
     return (
         <div className="apps-container">
+            <div className="apps-container__btn"><img src={arrow} alt="<<"/></div>
+            <div className="apps-container__slider">
             <Weather/>
+            </div>
+            <div className="apps-container__btn"><img src={arrow} alt=">>"/></div>
+
         </div>
     );
 }
