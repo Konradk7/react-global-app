c4e2a5c51b89508ad4ca33c7fbbb31881596792e
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-18 13:04:23 +0200

    Init lists elemnts style

--- src/components/NewMeal.js
+++ src/components/NewMeal.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Meal from "./Meal";
+import plus from "../images/plus.png";
 
 function NewMeal({onNewMeal}) {
     const [title, setTitle] = useState([]);
@@ -7,15 +8,16 @@ function NewMeal({onNewMeal}) {
 
     const handleAddTitle = e => {
             setTitle(prevState => [...prevState, value]);
+            setValue('');
     }
     const handleAddItem = e => {
         setValue(e.target.value)
     }
 
     return (
-        <div>
-            <input type="text" value={value} onChange={handleAddItem} />
-            <button onClick={handleAddTitle}>Add Title</button>
+        <div className="fitness-title-container">
+            <input type="text" value={value} onChange={handleAddItem} placeholder="Add title..." className="fitness-title-container__input"/>
+            <button onClick={handleAddTitle} className="fitness-title-container__button"><img src={plus} alt="Add"/></button>
             {title.map((title, idx) => {
                 return <ul key={idx}>{title}<Meal /></ul>
             })}
