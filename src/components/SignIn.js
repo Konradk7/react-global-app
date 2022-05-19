fd2268afa8eaeb36902885527ac833bbfbb0dbe7
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-05-19 10:01:47 +0200

    add styles to signin/register

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,9 +4,9 @@ import Register from "./Register";
 function SignIn(props) {
     return (
         <div>
-            <form>
+
                 <Register />
-            </form>
+
         </div>
     );
 }
